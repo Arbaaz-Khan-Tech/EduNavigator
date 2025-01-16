f91fdbea2d3c5add0c87fc22d74aa254a62dd449
BaazBoy <arbukhan1971@gmail.com>
2025-01-17 00:14:01 +0530

    Modified UI and Fixed Minor Issue

--- public/js/loco.js
+++ public/js/loco.js
@@ -1,5 +1,5 @@
 const scroll = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),  // Correctly target the scroll container
+  el: document.querySelector('[locomo]'),  // Correctly target the scroll container
   smooth: true,
   direction: 'horizontal'  // Set the scroll direction to horizontal
 });
